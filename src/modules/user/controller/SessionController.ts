8bf5df96acaeacd4201fd8e2bb28a68484035644
Pedro <pedro-gomes17@hotmail.com>
2020-12-31 11:16:29 -0300

    Adicionado biblioteca class-transformer para possibilitar excluir dados que não sejam retornados

--- src/modules/user/controller/SessionController.ts
+++ src/modules/user/controller/SessionController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 import AuthenticateUserService from '../services/AuthenticateUserService';
 
 class SessionController {
@@ -13,10 +14,8 @@ class SessionController {
       password,
     });
 
-    delete user.password;
-
     return response.json({
-      user,
+      user: classToClass(user),
       token,
     });
   }
