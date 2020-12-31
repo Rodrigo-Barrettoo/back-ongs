8822d5972c7761a85798d0bc7394620abeb37099
Pedro <pedro-gomes17@hotmail.com>
2020-12-31 11:27:21 -0300

    server

--- src/shared/server.ts
+++ src/shared/server.ts
@@ -21,6 +21,7 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
       .status(err.statusCode)
       .json({ status: 'error', message: err.message });
   }
+
   return response
     .status(500)
     .json({ status: 'error', message: 'Internal server error' });
